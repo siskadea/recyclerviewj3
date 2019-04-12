7e265ea92fc84222ed350cf74c926080ec739ce6
Dedek <siskadea.siskhadhea@gmail.com>
2019-04-12 13:45:09 +0700

    recyclerviewj3

---
+++ RecyclerView/app/src/main/java/com/siska/recyclerview/MainActivity.java
@@ -0,0 +1,34 @@
+package com.siska.recyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private MahasiswaAdapter adapter;
+    private ArrayList<Mahasiswa> mahasiswaArrayList;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        addData();
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        adapter = new MahasiswaAdapter(mahasiswaArrayList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+    void  addData(){
+        mahasiswaArrayList = new ArrayList<>();
+        mahasiswaArrayList.add(new Mahasiswa("Dea", "1731710146", "085"));
+        mahasiswaArrayList.add(new Mahasiswa("Meta", "1731710146", "082"));
+
+    }
+}
